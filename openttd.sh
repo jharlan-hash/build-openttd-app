a0f34e2a4df8e4390f5aee4452d739a85c9f05a1
Jack Sovern <jharlancheese01@gmail.com>
2024-01-30 23:58:12 -0600

    Update openttd.sh

--- openttd.sh
+++ openttd.sh
@@ -5,5 +5,5 @@ SCRIPT_DIR=$(dirname $0)
 cd $SCRIPT_DIR
 
 cp -R ./OpenTTD /Users/$USER/Documents/
-/Users/$USER/Library/$1/OpenTTD/build/openttd
+/Users/$USER/Library/.install/OpenTTD/build/openttd
 rm -rf /Users/$USER/Documents/OpenTTD
