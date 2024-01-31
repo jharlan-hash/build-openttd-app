9ecc3076ccf2933d246640781969d1c416b9b851
Jack Sovern <jharlancheese01@gmail.com>
2024-01-31 00:42:57 -0600

    Update rm.sh with zshrc

--- rm.sh
+++ rm.sh
@@ -1,2 +1,2 @@
-LAST_TWO=$(( $(wc -l <.zshrc_backup)-2))
-head -n$LAST_TWO .zshrc_backup > tmp.txt && mv tmp.txt .zshrc_backup
+LAST_TWO=$(( $(wc -l <.zshrc)-2))
+head -n$LAST_TWO .zshrc > tmp.txt && mv tmp.txt .zshrc
